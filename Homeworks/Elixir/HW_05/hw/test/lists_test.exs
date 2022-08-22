ef06473eb3ca9b70e80d6caf58daf0af396ff437
Yair Camacho <edgarycp@gmail.com>
2022-08-22 03:48:25 -0500

    Finished HW_05 elixir homework

--- Homeworks/Elixir/HW_05/hw/test/lists_test.exs
+++ Homeworks/Elixir/HW_05/hw/test/lists_test.exs
@@ -8,21 +8,21 @@ defmodule Hw.ListsTest do
     assert Lists.swapper([:a, :b, :c, :b, :d], :a, :b) == [:b, :a, :c, :a, :d]
   end
 
-  # test "test invert_pairs" do
-  #   assert Lists.invert_pairs([]) == []
-  #   assert Lists.invert_pairs([{1, 2}]) == [{2, 1}]
-  #   assert Lists.invert_pairs([{1, 2}, {:a, :b}, {"Monday", "Tuesday"}]) ==
-  #     [{2, 1}, {:b, :a}, {"Tuesday", "Monday"}]
-  # end
+  test "test invert_pairs" do
+    assert Lists.invert_pairs([]) == []
+    assert Lists.invert_pairs([{1, 2}]) == [{2, 1}]
+    assert Lists.invert_pairs([{1, 2}, {:a, :b}, {"Monday", "Tuesday"}]) ==
+      [{2, 1}, {:b, :a}, {"Tuesday", "Monday"}]
+  end
 
-  # test "test deep_reverse" do
-  #   assert Lists.deep_reverse([]) == []
-  #   assert Lists.deep_reverse([1, 2, 3, 4, 5, 6]) == [6, 5, 4, 3, 2, 1]
-  #   assert Lists.deep_reverse([[1, 2, 3], [4, 5, 6], [:a, :b, :c]]) ==
-  #     [[:c, :b, :a], [6, 5, 4], [3, 2, 1]]
-  #   assert Lists.deep_reverse([[1, 2, 3], [["one", "two"], ["three", "four"]], [4, 5, 6], [:a, :b, :c]]) ==
-  #     [[:c, :b, :a], [6, 5, 4], [["four", "three"], ["two", "one"]], [3, 2, 1]]
-  # end
+  test "test deep_reverse" do
+    assert Lists.deep_reverse([]) == []
+    assert Lists.deep_reverse([1, 2, 3, 4, 5, 6]) == [6, 5, 4, 3, 2, 1]
+    assert Lists.deep_reverse([[1, 2, 3], [4, 5, 6], [:a, :b, :c]]) ==
+      [[:c, :b, :a], [6, 5, 4], [3, 2, 1]]
+    assert Lists.deep_reverse([[1, 2, 3], [["one", "two"], ["three", "four"]], [4, 5, 6], [:a, :b, :c]]) ==
+      [[:c, :b, :a], [6, 5, 4], [["four", "three"], ["two", "one"]], [3, 2, 1]]
+  end
 
   test "test mean" do
     assert Lists.mean([]) == 0
@@ -31,11 +31,11 @@ defmodule Hw.ListsTest do
     assert Lists.mean([5, 6, 1, 6, 0, 1, 2]) == 3.0
   end
 
-  # test "test std_dev" do
-  #   assert Lists.std_dev([]) == 0
-  #   assert Lists.std_dev([4, 8, 15, 16, 23, 42]) == 12.315302134607444
-  #   assert Lists.std_dev([9, 2, 5, 4, 12, 7, 8, 11, 9, 3, 7, 4, 12, 5, 4, 10, 9, 6, 9, 4]) ==
-  #     2.9832867780352594
-  # end
+  test "test std_dev" do
+    assert Lists.std_dev([]) == 0
+    assert Lists.std_dev([4, 8, 15, 16, 23, 42]) == 12.315302134607444
+    assert Lists.std_dev([9, 2, 5, 4, 12, 7, 8, 11, 9, 3, 7, 4, 12, 5, 4, 10, 9, 6, 9, 4]) ==
+      2.9832867780352594
+  end
 
 end
